e4efff0a134850a9ae5a93c8c470c434a3756453
Sigl4S <lucas.schlaich@web.de>
2025-01-03 06:41:13 +0100

    added Webstorage API

--- 05HTML_JavaScript.js
+++ 05HTML_JavaScript.js
@@ -63,3 +63,19 @@ function drop(ev) {
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
 }
+
+//------------------------------------------------
+//Webstorage Tryout
+//------------------------------------------------
+function clickCounter() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
